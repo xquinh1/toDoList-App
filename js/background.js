67a5ea4f6c3440fbf1f8fc3c4a84090c0fe9041a
NgX Quinh <xquinh1@gmail.com>
2022-02-21 20:59:33 +0700

    update background.js

--- js/background.js
+++ js/background.js
@@ -1,10 +1,20 @@
-const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg"];
+// const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg"];
 
-const chosenImage = images[Math.floor(Math.random() * images.length)];
+// const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+// bgImage.src = `img/${chosenImage}`;
 
 const bgImage = document.querySelector("img");
 
-bgImage.src = `img/${chosenImage}`;
+const clientID = "kKF-JLSWQ6V8_I6343JL7ngZJmELfUfHOOXf5CfR718";
 
 
+const endpoint = `https://api.unsplash.com/photos/?client_id=${clientID}`;
+console.log(endpoint);
+
+fetch(endpoint)
+    .then(response => response.json())
+    .then(data => {
+        bgImage.src = data[0].urls.regular;
+    })
 
